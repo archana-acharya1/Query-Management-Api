630cd9b1d25aedff4144f2bd77502d2fb92db4a8
archana-acharya1 <145975347+archana-acharya1@users.noreply.github.com>
2025-03-16 16:14:40 +0545

    commit

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,26 @@
+import { Role } from '../enums/role.enum';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsEnum(Role)
+  @IsNotEmpty()
+  role: Role;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return this.prisma.user.create({
+      data: createUserDto,
+    });
+  }
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async findOne(id: string) {
+    return this.getUser(id);
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    await this.getUser(id);
+    return this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
+  }
+
+  async remove(id: string) {
+    await this.getUser(id);
+    return this.prisma.user.delete({
+      where: { id },
+    });
+  }
+
+  private async getUser(id: string) {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException('User not Found');
+    }
+    return user;
+  }
+}
